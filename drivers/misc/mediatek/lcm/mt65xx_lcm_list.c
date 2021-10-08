75acc5e8c448d00305ff82b7417bc407c8452d1c
jmpfbmx <jmpf.bmx@gmail.com>
2021-10-08 20:15:12 +0200

    ac50fnev2: Import ili9806e_dsi_vdo_fwvga_ivo_cw_tn_d5270 LCM driver and enable it
     * Decoded by @jmpfbmx && @Ruben1863
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I73561dc12aeaaa7f8c777a6e8bfbc66a321055d1

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
@@ -34,6 +34,10 @@ LCM_DSI_MODE_CON lcm_dsi_mode;
 #endif
 
 LCM_DRIVER *lcm_driver_list[] = {
+#if defined(ILI9806E_DSI_VDO_FWVGA_IVO_CW_TN_D5270)
+	&ili9806e_dsi_vdo_fwvga_ivo_cw_tn_d5270_lcm_drv,
+#endif
+
 #if defined(OTM1284A_HD720_DSI_VDO_TM)
 	&otm1284a_hd720_dsi_vdo_tm_lcm_drv,
 #endif
